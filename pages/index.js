956e36346ba6d033fb8c9bb74354ee3d4e9bf38e
Jawad <jkabir.bscs21seecs@seecs.edu.pk>
2023-06-04 12:57:46 +0500

    Created the AppBar

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Test from '@/components/Test'
+import Table from '@/components/Table'
+import AppBar from '@/components/AppBar'
+
+
+export default function Home() {
+  return (
+    <>
+    <AppBar />
+    </>
+      )
+}
