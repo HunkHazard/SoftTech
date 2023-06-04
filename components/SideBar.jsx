59a8aae41c923a968770fa19bf3c02fd8c67287c
Jawad <jkabir.bscs21seecs@seecs.edu.pk>
2023-06-04 15:41:40 +0500

    added sidebar and generic graphs for now

---
+++ components/SideBar.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import NavigationIcon from "@mui/icons-material/Navigation";
+import { useState } from "react";
+
+
+export default function SideBar() {
+  const [selected, setSelected] = useState(-1);
+  return (
+    
+      <div
+        className="drawer"
+        style={{ height: "92vh", width: "20vw", backgroundColor: "#bdbdbd" }}
+      >
+        <CssBaseline />
+
+        <Box sx={{ overflow: "auto" }}>
+          <List>
+            {["Station 1", "Station 2", "Station 3", "Station 4"].map(
+              (text, index) => (
+                <ListItem
+                  key={text}
+                  style={{
+                    boxSizing: "border-box",
+                    padding: "0px",
+                    
+                    marginTop: "20px",
+                    
+                    borderBottom:
+                      selected === index ? "5px solid Yellow" : "none",
+                  }}
+                  onClick={() => {
+                    setSelected((prev) => {
+                      return index;
+                    });
+                  }}
+                >
+                  <ListItemButton style={{ backgroundColor: "#e0e0e0" }}>
+                    <ListItemIcon>
+                      <NavigationIcon style={{ transform: "rotate(90deg)" }} />
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItemButton>
+                </ListItem>
+              )
+            )}
+          </List>
+        </Box>
+      </div>
+
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,60 @@
-import Test from '@/components/Test'
-import Table from '@/components/Table'
-import AppBar from '@/components/AppBar'
-
+import AppBar from "@/components/AppBar";
+import SideBar from "@/components/SideBar";
+import GraphArea from "@/components/GraphArea";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
     <>
-    <AppBar />
+      <AppBar />
+      <div className="body" style={{ display: "flex", direction: "row" }}>
+        <SideBar />
+        <div
+          className="graphArea"
+          style={{
+            width: "100vw",
+            height: "auto",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <div
+            id="graph2"
+            style={{
+              width: "40vh",
+              height: "30vh",
+              backgroundColor: "#bdbdbd",
+              marginTop: "1vh",
+            }}
+          >
+            <Typography variant="h4" color="initial">
+              Field 1
+            </Typography>
+            <GraphArea />
+            <Typography variant="h6" color="initial">
+              Power Consumption
+            </Typography>
+          </div>
+          <div
+            id="graph2"
+            style={{
+              width: "40vh",
+              height: "30vh",
+              backgroundColor: "#bdbdbd",
+              marginTop: "1vh",
+            }}
+          >
+            <Typography variant="h4" color="initial">
+              Field 2
+            </Typography>
+            <GraphArea />
+            <Typography variant="h6" color="initial">
+              Power Consumption
+            </Typography>
+          </div>
+        </div>
+      </div>
     </>
-      )
+  );
 }
