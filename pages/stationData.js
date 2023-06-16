46017c8e7f867885721a1bc6c041afc76f537b08
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2023-06-16 19:56:20 +0500

    created a new stationData.js file

---
+++ pages/stationData.js
@@ -0,0 +1,78 @@
+export const stationData = [
+  {
+    name: "Station 1",
+    current_reading: {
+      rate_one: 1990,
+      rate_two: 2000,
+    },
+    energy_consumption: {
+      low: 345367,
+      normal: 789456,
+    },
+    field_one: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [4, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+    field_two: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    },
+  },
+  {
+    name: "Station 2",
+    current_reading: {
+      rate_one: 987,
+      rate_two: 654,
+    },
+    energy_consumption: {
+      low: 123456,
+      normal: 789012,
+    },
+    field_one: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    },
+    field_two: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+    },
+  },
+  {
+    name: "Station 3",
+    current_reading: {
+      rate_one: 13579,
+      rate_two: 24680,
+    },
+    energy_consumption: {
+      low: 975310,
+      normal: 864209,
+    },
+    field_one: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [2, 4, 6, 8, 10, 8, 6, 4, 2, 0],
+    },
+    field_two: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [1, 3, 5, 7, 9, 10, 8, 6, 4, 2],
+    },
+  },
+  {
+    name: "Station 4",
+    current_reading: {
+      rate_one: 1111,
+      rate_two: 2222,
+    },
+    energy_consumption: {
+      low: 333333,
+      normal: 444444,
+    },
+    field_one: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [5, 4, 3, 2, 1, 1, 2, 3, 4, 5],
+    },
+    field_two: {
+      x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      y: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    },
+  },
+];
